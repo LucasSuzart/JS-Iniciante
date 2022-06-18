3b414787f70c9582e4904c5438ccfa4456d0216c
Lucas Suzart <89870289+LucasSuzart@users.noreply.github.com>
2022-06-18 01:49:18 -0300

    Update 1-MeuPrimeiroPrograma.js

--- 1-MeuPrimeiroPrograma.js
+++ 1-MeuPrimeiroPrograma.js
@@ -1,14 +1,7 @@
 console.log('teste git')
 
-const idade = 26
+const idade = 50
 const idade2 = 20
 
 console.log('idade = ', idade)
 
-console.log(idade + 2)
-
-console.log(idade + idade2)
-
-console.log('idade = ', idade + idade2)
-
-console.log('excluir')
